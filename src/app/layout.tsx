cac9249bcf3e0a688a38282f0fcc9917c575d490
longdoheun <george1119@naver.com>
2023-10-14 16:17:29 +0900

    og tag image changed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,28 +3,14 @@ import type { Metadata } from "next";
 import { Inter, Source_Serif_4 } from "next/font/google";
 import "../../public/static/fonts/fonts.css";
 import "./globals.css";
-import TITLE from "../../public/static/image/title.png";
 
 const inter = Inter({ subsets: ["latin"] });
 const SourceSerif = Source_Serif_4({ subsets: ["cyrillic"] });
 
-const meta = {
-  type: "website",
-  url: "https://the-15th-grand-epilogue.vercel.app/",
+export const metadata: Metadata = {
   title: "The 15th Grand Epilogue",
   description:
     "글로벌경제학과 15주년 기념 게필로그 행사에 글로벌경제학과 가족분들을 모십니다.",
-  siteName: "Grand Epilogue",
-  images: [
-    {
-      url: "/static/image/title.png",
-    },
-  ],
-};
-// meta data will be shown as mobile card
-export const metadata: Metadata = {
-  title: meta.title,
-  description: meta.description,
   openGraph: {
     type: "website",
     url: "https://the-15th-grand-epilogue.vercel.app/",
@@ -34,7 +20,7 @@ export const metadata: Metadata = {
     siteName: "Grand Epilogue",
     images: [
       {
-        url: "/static/image/title.png",
+        url: "/static/image/og-image.png",
       },
     ],
   },
