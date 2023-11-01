c344e31a0d537ac52c537dcf041ebcecef320f97
longdoheun <george1119@naver.com>
2023-11-01 16:01:20 +0900

    invitation update

--- src/components/MainPage/MainInfo.tsx
+++ src/components/MainPage/MainInfo.tsx
@@ -1,11 +1,26 @@
+import Link from "next/link";
 import React from "react";
 
 export default function MainInfo() {
   return (
-    <div className="main-page-info">
-      <div>2023.11.24. FRI. 19:00 ~</div>
-      <div>SKKU 600th Anniversary building</div>
-      <div>Dress Code : Black & White</div>
-    </div>
+    <>
+      <div className="main-page-info">
+        <div>2023.11.24. FRI. 19:00 ~</div>
+        <div>SKKU 600th Anniversary building</div>
+        <div>Dress Code : Black & White</div>
+        <section className="sign-up">
+          <div className="underlined-text">
+            <Link href="https://forms.gle/WML4JrMorpQQXF1a9">
+              졸업생 신청하기
+            </Link>
+          </div>
+          <div className="underlined-text">
+            <Link href="https://forms.gle/kebi4MvyncEG5XEc7">
+              재학생 신청하기
+            </Link>
+          </div>
+        </section>
+      </div>
+    </>
   );
 }
