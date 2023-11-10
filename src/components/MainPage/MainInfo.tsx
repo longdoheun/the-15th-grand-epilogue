859981eac1c9d220681ef9011b1bd73478735444
longdoheun <george1119@naver.com>
2023-11-10 18:40:08 +0900

    info update

--- src/components/MainPage/MainInfo.tsx
+++ src/components/MainPage/MainInfo.tsx
@@ -5,7 +5,7 @@ export default function MainInfo() {
   return (
     <>
       <div className="main-page-info">
-        <div>2023.11.24. FRI. 19:00 ~</div>
+        <div>2023.11.24. FRI. 19:00 ~ 21:30</div>
         <div>SKKU 600th Anniversary building</div>
         <div>Dress Code : Black & White</div>
         <section className="sign-up">
